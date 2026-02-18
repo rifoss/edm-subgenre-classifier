1d3623b93f98fccddb3eaae5194c09dc5d6b7141
Jody <Jodysuryatna@gmail.com>
2026-02-18 15:39:14 -0500

    Day 53: Added feedback logic and refined UI

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def extract_features_v5_inference(file_path):
     """Mirroring the v5 Batch Extractor logic for 59 features."""
     try:
         # Load 30s sample from the middle (60s)
-        y, sr = librosa.load(file_path, sr=22050, offset=60, duration=30)
+        y, sr = librosa.load(file_path, sr=22050, offset=60, duration=30, res_type='kaiser_fast')
         
         # 1. Stats
         tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
